586fa15d7b1b72949bf03540a4392d46d91ac352
Sergei <delusional.drago@gmail.com>
2025-09-01 14:58:27 +0800

    Applying CSS

--- svitlo_monza/settings.py
+++ svitlo_monza/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'landing_page'
+    'landing_page',
+    'gallery',
+    'libs',
 ]
 
 MIDDLEWARE = [
